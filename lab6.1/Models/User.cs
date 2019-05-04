af3d8e479080daa3458924ade663709b66d56086
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2019-05-04 21:44:13 +0300

    I em faking finihed this faking validator (Lab6.1)

--- lab6.1/Models/User.cs
+++ lab6.1/Models/User.cs
@@ -13,34 +13,47 @@ namespace lab6._1.Models
         public int Id { get; set; }
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
-        
-        [Required]
+        [UIHint("EmailAddress")]
+        [Required(ErrorMessage ="Поле обов'язково повинно бути заповнене")]
+        //[Remote("CheckName", "Home", ErrorMessage = "Name is not valid.")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некоректний адрес")]
         public string Email { get; set; }
         [Display(Name ="Повне Ім'я")]
-        [Required]
+        [Required(ErrorMessage = "Поле обов'язково повинно бути заповнене")]
         public string FullName { get; set; }
         [Display(Name ="Ім'я для форумів")]
-        [Required]
+        [Required(ErrorMessage = "Поле обов'язково повинно бути заповнене")]
         public string ForumName { get; set; }
         [Display(Name ="Password")]
         [DataType(DataType.Password)]
-        [Required]
+        [UIHint("Password")]
+        [Required(ErrorMessage = "Поле обов'язково повинно бути заповнене")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,12}$", ErrorMessage = "Некоректний пароль (Aa-Zz and 0-9 and spezial char)")]
+        [StringLength(12, MinimumLength = 6, ErrorMessage = "Довжина рядка повинна бути від 6 до 12 символів")]
         public string Password { get; set; }
+        [Display(Name = "Password Confirm")]
+        [System.Web.Mvc.Compare("Password", ErrorMessage = "Пароли не співпадають")]
+        [DataType(DataType.Password)]
+        [UIHint("Password")]
+        [Required(ErrorMessage = "Поле обов'язково повинно бути заповнене")]
+        public string PasswordConfirm { get; set; }
         [Display(Name ="Стать")]
         public bool Sex { get; set; }
         [Display(Name = "Рік народження")]
-        [DataType(DataType.Date)]
-        public DateTime YearOfBirth { get; set; }
+        //[DataType(DataType.Date)]
+        [Range(1700, 2000, ErrorMessage = "Неприпустимий рік")]
+        public int YearOfBirth { get; set; }
         [Display(Name = "Освіта")]
         public string Education { get; set; }
         [Display(Name = "Місце проживання")]
         public string Location { get; set; }
         [Display(Name = "Чому ви вибрали саме нас")]
         [DataType(DataType.MultilineText)]
+        [UIHint("MultilineText")]
         public string Abouth { get; set; }
-        [Display(Name = " Я згоден зі всіа правилами")]
+        [Display(Name = " Я згоден зі всіма правилами")]
         [UIHint("Boolean")]
-        [Required]
+        [Required(ErrorMessage = "Поле обов'язково повинно бути заповнене")]
         public bool IConfirmed { get; set; }
     }
 }
